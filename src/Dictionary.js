336c6cea269263750149ac633426b149d6e58b28
adayeis <136622255+adayeis@users.noreply.github.com>
2023-12-27 20:42:21 +0200

    added api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,24 +1,47 @@
 import { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [definition, setDefinition] = useState({});
 
-  function alertKeyword(event) {
-    event.preventDefault();
-    alert(`Searcing ${keyword}`);
+  function searchMeaning(response) {
+    console.log(response.data);
+    setDefinition({
+      word: response.data.word,
+      phonetic: response.data.phonetic,
+      partOfSpeech: response.data.meanings[0].partOfSpeech,
+      definition: response.data.meanings[0].definition,
+      example: response.data.meanings[0].example,
+    });
+  }
+  function callDictionaryApi(event) {
+    if (keyword) {
+      event.preventDefault();
+      let apiKey = "67ct2f0dc4c74e3fcab1f74do85ff4a4";
+      let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+      axios.get(apiUrl).then(searchMeaning);
+    }
   }
 
   function setValue(event) {
     setKeyword(event.target.value);
-    console.log(keyword);
   }
+
   return (
     <div className="Dictionary">
-      <form onSubmit={alertKeyword}>
+      <form onSubmit={callDictionaryApi}>
         <input type="search" placeholder="Search" onChange={setValue} />
         <input type="submit" />
       </form>
+      <ul>
+        <li>{definition.word}</li>
+        <li>{definition.phonetic}</li>
+        <li>{definition.partOfSpeech}</li>
+        <li>{definition.definition}</li>
+        <li>{definition.example}</li>
+      </ul>
     </div>
   );
 }
