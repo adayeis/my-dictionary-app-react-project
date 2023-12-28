8e1f813448b3d70c61204a91051c802a57e1a281
adayeis <136622255+adayeis@users.noreply.github.com>
2023-12-28 23:57:14 +0200

    status not_found fix

--- src/DisplayDefinition.js
+++ src/DisplayDefinition.js
@@ -5,19 +5,23 @@ import "./DisplayDefinition.css";
 export default function DisplayDefinition(props) {
   if (props.result) {
     console.log(props.result);
-    return (
-      <div className="DisplayDefinition">
-        <h2>{props.result.word}</h2>
-        <h3>{props.result.phonetic}</h3>
-        {props.result.meanings.map(function (meaning, index) {
-          return (
-            <article key={index}>
-              <DisplayMultipleMeanings meaning={meaning} index={index} />
-            </article>
-          );
-        })}
-      </div>
-    );
+    if (props.result.status != "not_found") {
+      return (
+        <div className="DisplayDefinition">
+          <h2>{props.result.word}</h2>
+          <h3>{props.result.phonetic}</h3>
+          {props.result.meanings.map(function (meaning, index) {
+            return (
+              <article key={index}>
+                <DisplayMultipleMeanings meaning={meaning} index={index} />
+              </article>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
